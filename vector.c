7eba2315a77ab0a00af6404752ba945497ecd89b
Joseph Watts <josephcw@live.com>
2018-10-01 18:58:13 -0700

    Complete assignment

--- vector.c
+++ vector.c
@@ -7,15 +7,32 @@ void vinit(vector * v) {
 }
 
 int vadd(vector * v, int value) {
-	// if it is full, 
-	if ()
-	
-	// increase the number of elements by 1.
+	// if it is full, double the size of the collection
+	if ((*v).maxCapacity == (*v).numElements) {
+		//int * tmpData = malloc(2 * (*v).maxCapacity * sizeof(int));
+		(*v).data = realloc((*v).data, 2 * (*v).maxCapacity * sizeof(int));
+		// Increase the maxCapacity variable
+		(*v).maxCapacity *= 2;
+	}
+
+	// Set the last value to be the newly inserted value.
+	(*v).data[(*v).numElements] = value;
+	// increase the number of taken elements by 1.
 	(*v).numElements += 1;
+	return 1;
 }
 
 int vrm(vector * v, int idx) {
-	return -1;
+	// If the index is too high return 0.
+	if (idx >= (*v).numElements) {
+		return 0;
+	}
+	// Remove value at specified index by shifting values down
+	for (int i = idx; i < (*v).numElements - 1; i++) {
+		(*v).data[i] = (*v).data[i+1];
+	}
+	// decrement the current number of held values
+	(*v).numElements -= 1;
 }
 
 void vcleanup(vector * v) {
@@ -23,11 +40,22 @@ void vcleanup(vector * v) {
 }
 
 int vget(vector v, int idx, int * result) {
-	return -1;
+	// If index is allowed set value of reslt paramater
+	// to be the value at the index and return 1.
+	// otherwise return 0.
+	if (idx >= v.numElements) {
+		return 0;
+	}
+	*result = v.data[idx];
+	return 1;
 }
 
 int vset(vector v, int idx, int value) {
-	return -1;
+	// If the index is too large, return 0
+	if (idx >= v.numElements) {
+		return 0;
+	}
+	v.data[idx] = value;
 }
 
 int vsize(vector v) {
