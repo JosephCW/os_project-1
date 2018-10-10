5593f778d1d69921dd0ee5d480713eeda2036dee
Joseph Watts <josephcw@live.com>
2018-10-09 20:05:35 -0500

    vget method

--- vector.c
+++ vector.c
@@ -54,6 +54,13 @@ void vcleanup(vector * v) {
 }
 
 int vget(vector v, int idx, int * result) {
+	// If index is allowed set value of reslt paramater
+	// to be the value at the index and return 1.
+	// otherwise return 0.
+	if (idx >= v.numElements) {
+		return 0;
+	}
+	*result = v.data[idx];
 	return 1;
 }
 
